206210316597891957ae172aae0c027b5527fe15
Anusha Basavaraja <78228113+anushab97@users.noreply.github.com>
2022-10-12 12:31:06 -0600

    Add files

---
+++ Lab 5/Part 1/startup-script.sh
@@ -0,0 +1,26 @@
+"""
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip git
+git clone https://github.com/cu-csci-4253-datacenter/flask-tutorial
+cd ~/flask-tutorial
+sudo python3 setup.py install
+sudo pip3 install -e .
+
+
+export FLASK_APP=flaskr
+flask init-db
+nohup flask run -h 0.0.0.0 &
+"""
+
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip git
+git clone https://github.com/cu-csci-4253-datacenter/flask-tutorial
+cd /flask-tutorial
+sudo python3 /flask-tutorial/setup.py install
+sudo pip3 install -e /flask-tutorial
+
+
+export FLASK_APP=/flask-tutorial/flaskr
+cat $FLASK_APP >> output
+flask init-db
+nohup flask run -h 0.0.0.0 &
